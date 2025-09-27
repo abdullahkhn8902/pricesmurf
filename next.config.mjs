9cf660ef65a80e5eb78cd168c4299261b75daf0b
abdullahkhn8902 <abdullahkhn8902@gmail.com>
2025-09-27 13:03:34 +0500

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || {};
+      config.externals['@google-cloud/secret-manager'] =
+        'commonjs @google-cloud/secret-manager';
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
